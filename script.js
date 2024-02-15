7427fe55c3af203f747c28a627a8e25734e22048
Amarjeet Aryan <74115660+alpha-metus@users.noreply.github.com>
2024-02-15 15:20:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const phrases = ["Graphics Designer", "UI/UX"];
+let currentPhrase = 0;
+let currentLetter = 0;
+let deleting = false;
+const typingSpeed = 200;
+const deletingSpeed = 100;
+const dynamicText = document.getElementById("dynamic-text");
+
+function type() {
+  let currentText = phrases[currentPhrase];
+  if (!deleting) {
+    dynamicText.textContent = currentText.substring(0, currentLetter + 1);
+    currentLetter++;
+    if (currentLetter === currentText.length) {
+      deleting = true;
+      setTimeout(type, 2000); // Wait a bit before start deleting
+    } else {
+      setTimeout(type, typingSpeed);
+    }
+  } else {
+    dynamicText.textContent = currentText.substring(0, currentLetter - 1);
+    currentLetter--;
+    if (currentLetter === 0) {
+      deleting = false;
+      currentPhrase = (currentPhrase + 1) % phrases.length; // Move to the next phrase
+    }
+    setTimeout(type, deletingSpeed);
+  }
+}
+
+// Start typing effect on page load
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(type, typingSpeed);
+});
